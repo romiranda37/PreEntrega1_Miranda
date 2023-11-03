6b3ba5216808780dc1530c578d65c435c0dbd258
romiranda37 <131496991+romiranda37@users.noreply.github.com>
2023-11-02 22:52:49 -0300

    primer commit

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      {/* Resto de tu contenido de la aplicación */}
+    </div>
+  );
+}
+export default App;
